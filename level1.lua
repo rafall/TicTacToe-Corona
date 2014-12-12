b85bcae97f84fd537819b3c3f4c7b82974c9ab80
Hiroki Minami <rafael.hiroki@gmail.com>
2014-12-12 02:17:48 -0200

    Adding tapping verification

--- level1.lua
+++ level1.lua
@@ -12,6 +12,37 @@ local scene = composer.newScene()
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
 
+local function myTapListener( event )
+
+    -- code executed when the button is tapped
+    if (event.y < display.contentHeight/3) then
+    	if (event.x < display.contentWidth/3) then
+    		print( " 1 " )
+		elseif (event.x > 2*display.contentWidth/3) then
+			print( " 3 " )
+		else
+			print( " 2 " )
+		end
+	elseif (event.y > 2*display.contentHeight/3) then
+		if (event.x < display.contentWidth/3) then
+    		print( " 7 " )
+		elseif (event.x > 2*display.contentWidth/3) then
+			print( " 9 " )
+		else
+			print( " 8 " )
+		end
+	else
+		if (event.x < display.contentWidth/3) then
+    		print( " 4 " )
+		elseif (event.x > 2*display.contentWidth/3) then
+			print( " 6 " )
+		else
+			print( " 5 " )
+		end
+	end
+		
+end
+
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
@@ -32,21 +63,25 @@ function scene:create( event )
 	lineY = 0
 	local vLine1 = display.newLine( lineX, lineY, lineX, lineY + display.contentHeight )
 	vLine1.strokeWidth = 15
-	local vLine1 = display.newLine( lineX*2,lineY, lineX*2, lineY + display.contentHeight )
+	vLine1 = display.newLine( lineX*2,lineY, lineX*2, lineY + display.contentHeight )
 	vLine1.strokeWidth = 15
 	-- adding horizontal lines
 	lineX = 0
 	lineY = display.contentHeight/3
-	local vLine1 = display.newLine( lineX, lineY, lineX + display.contentWidth , lineY )
+	vLine1 = display.newLine( lineX, lineY, lineX + display.contentWidth , lineY )
 	vLine1.strokeWidth = 15
-	local vLine1 = display.newLine( lineX,lineY*2, lineX + display.contentWidth, lineY*2 )
+	vLine1 = display.newLine( lineX,lineY*2, lineX + display.contentWidth, lineY*2 )
 	vLine1.strokeWidth = 15
+
+	-- adding event listener to the background
+	background:addEventListener( "tap", myTapListener )
 
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
 	sceneGroup:insert( vLine1 )
 
 end
+
 
 
 function scene:show( event )
